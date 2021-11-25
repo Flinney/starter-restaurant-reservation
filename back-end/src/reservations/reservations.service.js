412be615d6f97ce7d84a60adaee92df1616cd43b
flinney <77643261+Flinney@users.noreply.github.com>
2021-11-24 20:29:36 -0600

    add mobile search api

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -44,6 +44,15 @@ function finish(reservation_id) {
     .update({ status: "finished" });
 }
 
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
 module.exports = {
   list,
   create,
@@ -51,4 +60,5 @@ module.exports = {
   read,
   finish,
   update,
+  search,
 };
