045cd13ac10ec5fbe4d1a77e8f70a55d8c5f2639
flinney <77643261+Flinney@users.noreply.github.com>
2021-11-29 18:03:19 -0600

    Use same form component for create and edit res.

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -4,6 +4,7 @@ import { createReservation } from "../utils/api";
 import { isNotOnTuesday } from "../utils/date-time";
 import { isInTheFuture } from "../utils/date-time";
 import ErrorAlert from "../layout/ErrorAlert";
+import Form from "./Form";
 
 export default function Reservations() {
   const history = useHistory();
@@ -51,86 +52,14 @@ export default function Reservations() {
     return () => controller.abort();
   };
 
-  const handleCancel = () => {
-    history.goBack();
-  };
-
   return (
     <>
       <ErrorAlert error={reservationsError} />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="first_name"
-          className="form-control"
-          id="first_name"
-          placeholder="First Name"
-          value={formData.first_name}
-          onChange={handleFormChange}
-          required
-        />
-        <input
-          type="text"
-          name="last_name"
-          className="form-control"
-          id="last_name"
-          placeholder="Last Name"
-          value={formData.last_name}
-          onChange={handleFormChange}
-          required
-        />
-        <input
-          type="tel"
-          name="mobile_number"
-          className="form-control"
-          id="mobile_number"
-          placeholder="Mobile Number"
-          value={formData.mobile_number}
-          onChange={handleFormChange}
-          required
-        />
-        <input
-          type="number"
-          name="people"
-          className="form-control"
-          id="people"
-          placeholder="Number of guests"
-          value={formData.people}
-          onChange={handleFormChange}
-          required
-          min="1"
-        />
-        <input
-          type="date"
-          name="reservation_date"
-          className="form-control"
-          id="reservation_date"
-          placeholder="YYY-MM-DD"
-          value={formData.reservation_date}
-          onChange={handleFormChange}
-          required
-        />
-        <input
-          type="time"
-          name="reservation_time"
-          className="form-control"
-          id="reservation_time"
-          placeholder="HH:MM"
-          value={formData.reservation_time}
-          onChange={handleFormChange}
-          required
-        />
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={handleCancel}
-        >
-          Cancel
-        </button>
-      </form>
+      <Form
+        initialformData={formData}
+        handleFormChange={handleFormChange}
+        handleSubmit={handleSubmit}
+      />
     </>
   );
 }
